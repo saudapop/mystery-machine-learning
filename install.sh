edf9d27fe5346306fdcba418f289bf79cdbbd8f7
Saud Ahmed <saud.ahmed@datarobot.com>
2019-08-26 18:40:14 -0400

    fix script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-echo "const API_TOKEN = $1; module.exports = API_TOKEN;" | cat > constants.js;
+rm -rf constants.js;
+echo "const API_TOKEN = \"$1\"; module.exports = API_TOKEN;" | cat > constants.js;
 npm i;
 cd client;
 npm i;
